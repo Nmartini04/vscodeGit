4848ddd3d3dcfbe64911317b59eb25b861bb3259
nicolasM <n.martini@live.fr>
2021-01-08 11:47:19 +0100

    mon troisiemecommit

--- index.php
+++ index.php
@@ -11,6 +11,16 @@
     <header>
         <h1>Mon supersite</h1>
     </header>
+
+    <section>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid ipsa suscipit odio qui harum ratione
+            pariatur velit provident itaque ipsam nemo quod eum aspernatur fugit ducimus explicabo, et vel ad.</p>
+    </section>
+    <main>
+    <div class="container">
+    
+    </div>
+    </main>
 </body>
 
 </html>
