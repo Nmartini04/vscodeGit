cc7a2514c9af7199edb8b0eb3ee36e081c70f700
nicolasM <n.martini@live.fr>
2021-01-08 11:19:18 +0100

    mon troisieme commit

--- index.php
+++ index.php
@@ -11,6 +11,15 @@
     <header>
         <h1>Mon supersite</h1>
     </header>
+    <main>
+        <section>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi assumenda odio odit delectus voluptatibus
+                quidem iste ipsam voluptatem illum quisquam nobis libero necessitatibus ipsa, cum facilis veniam
+                cupiditate dolorem repellendus?
+            </p>
+        </section>
+    </main>
 </body>
 
 </html>
